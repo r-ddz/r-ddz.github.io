fe42d7fd15f91ea9a654836a47c8252367e059a7
瑞 <740660485@qq.com>
2020-02-07 03:22:04 +0800

    1

---
+++ demo/test/test.js
@@ -0,0 +1,24 @@
+var testUtil = {
+
+    test1: function () {
+        var a = {
+            name: '张三',
+            age: 18
+        };
+
+
+        var b = JSON.stringify(a);
+
+        var c = JSON.parse(b);
+
+
+
+
+
+    }
+
+
+
+
+
+}
